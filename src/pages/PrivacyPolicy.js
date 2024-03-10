ac4e06eb1907580a665d6eac40c571b253515e4a
abdulrahmanmahmood <abdulrahmanabdultawab189_sd@fcis.bsu.edu.eg>
2024-03-10 15:04:15 +0200

    edith the translation

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -43,166 +43,47 @@ function PrivacyPolicy() {
         handleProductClick={handleProductClick}
       />
 
-      <div className="green-containerr cartGreen mt-[200px]">
-        <div className="header-container">
-          <div className=" testtt">
+      <div className="bg-gray-50 bottom-0 overflow-y-hidden mt-[150px]">
+        <div className="mt-3 relative top-12 lg:top-0 lg:mx-12 overflow-y-hidden md:px-5  sm:px-2">
+          <div className="">
             <WhatsAppIcon />
-            <div className="changePasswordText">
-              <h2>Conditions générales</h2>
-              <h6>Bienvenue à Et Vitapara!</h6>
-              <h6>
-                Ces termes et conditions définissent les règles et
-                réglementations pour l'utilisation du site Et Vitapara,
-                disponible à l'adresse : https://vitaparapharma.com/.
-              </h6>
-              <h6>
-                En accédant à ce site, nous supposons que vous acceptez ces
-                termes et conditions. Ne continuez pas à utiliser Et Vitapara si
-                vous n'acceptez pas tous les termes et conditions mentionnés sur
-                cette page.
-              </h6>
-              <h6>
-                La définition suivante s'applique à ces termes et conditions, à
-                la déclaration de confidentialité, à l'avis de
-                non-responsabilité et à tous les accords : "Le client", "Vous"
-                fait référence à vous, la personne qui navigue sur ce site et
-                accepte les termes et conditions de la société. "La Société",
-                "Nous", fait référence à notre entreprise. "Partie", "Parties"
-                fait référence à la fois au client et à nous. Tous les termes se
-                réfèrent à l'offre, l'acceptation et le paiement nécessaires
-                pour mener à bien un processus d'assistance au client de la
-                manière la plus appropriée, dans le but de satisfaire les
-                besoins du client en ce qui concerne la fourniture des services
-                de la société mentionnés, conformément à et sous réserve de la
-                loi en vigueur. Toute utilisation des termes susmentionnés ou
-                d'autres mots au singulier ou au pluriel, ou en utilisant des
-                majuscules, et/ou lui/elle ou eux, est considérée comme
-                interchangeable et se réfère donc à la même chose.
-              </h6>
-              <h6>
-                Cookies Nous utilisons des cookies. En accédant à Et Vitapara,
-                vous avez accepté l'utilisation de cookies conformément à la
-                politique de confidentialité d'Et Vitapara.
-              </h6>
-              <h6>
-                La plupart des sites web interactifs utilisent des cookies pour
-                nous permettre de récupérer les détails de l'utilisateur pour
-                chaque visite. Notre site utilise des cookies pour activer
-                certaines fonctionnalités afin de faciliter la navigation pour
-                les personnes qui visitent notre site web. Certains de nos
-                partenaires affiliés/annonceurs peuvent également utiliser des
-                cookies.
-              </h6>
-              <h6>
-                Licence Sauf indication contraire, Et Vitapara et/ou ses
-                concédants de licence détiennent les droits de propriété
-                intellectuelle de tout le matériel présent sur Et Vitapara. Tous
-                les droits de propriété intellectuelle sont réservés. Vous
-                pouvez accéder à ceci depuis Et Vitapara pour votre usage
-                personnel tout en respectant les restrictions énoncées dans ces
-                termes et conditions.
-              </h6>
-              <h6>Vous ne devez pas :</h6>
-              <h6>Republier le contenu de Et Vitapara.</h6>
-              <h6>
-                Vendre, louer ou sous-licencier le contenu de Et Vitapara.
-              </h6>
-              <h6>
-                Reproduire, copier ou dupliquer le contenu de Et Vitapara.
-              </h6>
-              <h6>Distribuer le contenu de Et Vitapara.</h6>
-              <h6>
-                Cet accord prend effet à compter de sa date. Nos termes et
-                conditions ont été créés avec l'aide d'un générateur gratuit de
-                termes et conditions.
-              </h6>
-              <h6>
-                Des parties de ce site offrent aux utilisateurs la possibilité
-                de publier et d'échanger des opinions et informations dans
-                certaines zones du site. Et Vitapara ne filtre, n'édite, ne
-                publie ni ne révise les commentaires avant leur apparition sur
-                le site. Les commentaires ne reflètent pas les vues et opinions
-                de Et Vitapara, de ses agents et/ou de ses filiales. Les
-                commentaires reflètent les vues et opinions de la personne qui
-                publie ses propres vues et opinions. Dans la mesure permise par
-                la loi applicable, Et Vitapara ne sera pas responsable des
-                commentaires ou de toute responsabilité, dommages ou dépenses
-                causés et/ou subis à la suite de l'utilisation et/ou de la
-                publication et/ou de l'apparition des commentaires sur ce site.
-              </h6>
-              <h6>
-                Et Vitapara se réserve le droit de surveiller tous les
-                commentaires et de supprimer tout commentaire qui pourrait être
-                considéré comme inapproprié, offensant ou en violation de ces
-                termes et conditions.
-              </h6>
-              <h6>Vous garantissez et déclarez que :</h6>
-              <h6>
-                Les commentaires ne violent aucun droit de propriété
-                intellectuelle, y compris sans limitation les droits d'auteur,
-                brevets ou marques déposées d'un tiers ;
-              </h6>
-              <h6>
-                Les commentaires ne contiennent aucun matériel diffamatoire,
-                offensant, injurieux, inapproprié ou illégal constituant une
-                violation de la vie privée ;
-              </h6>
-              <h6>
-                Les commentaires ne seront pas utilisés pour solliciter ou
-                promouvoir des affaires ou des coutumes, ou pour mener des
-                activités commerciales ou illégales.
-              </h6>
-              <h6>
-                Par la présente, vous accordez à Et Vitapara une licence non
-                exclusive pour utiliser, reproduire, éditer et autoriser
-                d'autres à utiliser, reproduire et éditer n'importe lequel de
-                vos commentaires sous n'importe quelle forme, format ou médium.
-              </h6>
-              <h6>Le lien hypertexte vers notre contenu</h6>
-              <h6>
-                Les organisations suivantes peuvent créer un lien vers notre
-                site web sans autorisation écrite préalable :
-              </h6>
-              <h6>Les agences gouvernementales ;</h6>
-              <h6>Les moteurs de recherche ;</h6>
-              <h6>Les organismes de presse ;</h6>
-              <h6>
-                Les distributeurs d'annuaires en ligne peuvent créer un lien
-                vers notre site de la même manière qu'ils le font vers les sites
-                web d'autres entreprises répertoriées ; et
-              </h6>
-              <h6>
-                Les entreprises accréditées au niveau du système à l'exception
-                des organisations à but non lucratif sollicitant des dons, des
-                centres commerciaux caritatifs, et des groupes collectant des
-                dons caritatifs qui ne peuvent pas créer un lien vers notre site
-                web.
-              </h6>
-              <h6>
-                Ces organisations peuvent créer un lien vers notre page
-                d'accueil, nos publications ou d'autres informations sur le site
-                web tant que le lien : (a) n'est en aucun cas trompeur ; (b)
-                n'implique pas à tort le parrainage, l'approbation ou
-                l'approbation de la partie liée et de ses produits et/ou
-                services ; et (c) convient au contexte du site de la partie
-                liée.
-              </h6>
-              <h6>
-                Nous pourrions considérer et approuver d'autres demandes de
-                liaison provenant des types d'organisations suivants :
-              </h6>
-              <h6>
-                sources d'information connues des consommateurs et/ou des
-                entreprises ;
-              </h6>
-              <h6>sites communautaires dot.com ;</h6>
-              <h6>
-                associations ou autres groupes représentant des œuvres
-                caritatives ;
-              </h6>
-              <h6>distributeurs d'annuaires électroniques ;</h6>
-              <h6>portails électroniques ;</h6>
-              <h6>cabinets de comptabilité, de droit et de conseil ; et</h6>
+            <div className="mb-5">
+              <h2>{translations[language]?.termsand}</h2>
+              <h6>{translations[language]?.welcometo}</h6>
+              <h6>{translations[language]?.adjust}</h6>
+              <h6>{translations[language]?.assume}</h6>
+              <h6>{translations[language]?.definition}</h6>
+              <h6>{translations[language]?.accessing}</h6>
+              <h6>{translations[language]?.most}</h6>
+              <h6>{translations[language]?.license}</h6>
+              <h6>{translations[language]?.mustnot}</h6>
+              <h6>{translations[language]?.repuplish}</h6>
+              <h6>{translations[language]?.sell}</h6>
+              <h6>{translations[language]?.repro}</h6>
+              <h6>{translations[language]?.distribute}</h6>
+              <h6> {translations[language]?.agreement}</h6>
+              <h6>{translations[language]?.avail}</h6>
+              <h6>{translations[language]?.reserve}</h6>
+              <h6>{translations[language]?.you}</h6>
+              <h6>{translations[language]?.comment}</h6>
+              <h6>{translations[language]?.les}</h6>
+              <h6>{translations[language]?.lestwo}</h6>
+              <h6>{translations[language]?.grant}</h6>
+              <h6>{translations[language]?.hyper}</h6>
+              <h6>{translations[language]?.may}</h6>
+              <h6>{translations[language]?.agenc}</h6>
+              <h6>{translations[language]?.search}</h6>
+              <h6>{translations[language]?.press}</h6>
+              <h6>{translations[language]?.online}</h6>
+              <h6>{translations[language]?.Systematic}</h6>
+              <h6>{translations[language]?.these}</h6>
+              <h6>{translations[language]?.cons}</h6>
+              <h6>{translations[language]?.know}</h6>
+              <h6>{translations[language]?.dotcom}</h6>
+              <h6>{translations[language]?.group}</h6>
+              <h6>{translations[language]?.electronic}</h6>
+              <h6>{translations[language]?.portals}</h6>
+              <h6>{translations[language]?.account}</h6>
               <h6>
                 établissements d'enseignement et associations commerciales.
               </h6>
@@ -325,9 +206,8 @@ function PrivacyPolicy() {
             </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-
     </div>
   );
 }
